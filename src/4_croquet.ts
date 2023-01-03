6faaad1a7358362d4ae1a4eb151389d1033d452b
Kay Sharrock <sharrock.kay@gmail.com>
2023-01-03 15:07:02 +0000

    Initial commit starting adventure.
    Also tidied up code formatting and a typo.

--- src/4_croquet.ts
+++ src/4_croquet.ts
@@ -1,6 +1,6 @@
-import { endAdventure } from '..';
-import { meetTheQueen } from './5_queen_of_hearts';
-import { askQuestion, clear, print } from '../console';
+import {endAdventure} from '..';
+import {meetTheQueen} from './5_queen_of_hearts';
+import {askQuestion, clear, print} from '../console';
 
 // 💡 FUN FACT: In Wonderland, croquet is played by using a flamingo 🦩
 //              to push a hedgehog 🦔 through an arch made of playing cards 🃏
@@ -22,45 +22,45 @@ type Mallet = Flamingo | Hammer | CroquetMallet;
 
 // 👉 FIXME ❌ We're defining a TBall and a TMallet, but not specifying anything about them...
 interface Croquet<TBall, TMallet> {
-	hit: (ball: TBall, mallet: TMallet) => boolean;
+    hit: (ball: TBall, mallet: TMallet) => boolean;
 }
 
 export function playCroquet(): void {
-	clear(true);
-	print('The Queen of Hearts 👑❤ invites you to play croquet 🏑');
+    clear(true);
+    print('The Queen of Hearts 👑❤ invites you to play croquet 🏑');
 
-	// 👉 FIXME ❌ Our ball and mallet are made of numbers.
-	const game: Croquet<number, number> = {
-		hit: (ball, mallet) => {
-			let score = 0;
+    // 👉 FIXME ❌ Our ball and mallet are made of numbers.
+    const game: Croquet<number, number> = {
+        hit: (ball, mallet) => {
+            let score = 0;
 
-			// 💡 Remember TypeScript only exists at compile time, so when this code *runs* the type we want (Ball)
-			// should appear to be a string with value 'Hedgehog'.
-			// ⚠️ Unlike strongly typed languages, there's no such thing as `typeof Ball` in JavaScript.
-			if (typeof ball === 'string') {
-				if (ball === 'Hedgehog') score += 50;
-			}
+            // 💡 Remember TypeScript only exists at compile time, so when this code *runs* the type we want (Ball)
+            // should appear to be a string with value 'Hedgehog'.
+            // ⚠️ Unlike strongly typed languages, there's no such thing as `typeof Ball` in JavaScript.
+            if (typeof ball === 'string') {
+                if (ball === 'Hedgehog') score += 50;
+            }
 
-			// 💡 Remember TypeScript only exists at compile time, so when this code *runs* the type we want (Mallet)
-			// should appear to be a string with value 'Flamingo'
-			// ⚠️ Unlike strongly typed languages, there's no such thing as `typeof Mallet` in JavaScript.
-			if (typeof mallet === 'string') {
-				if (mallet === 'Flamingo') score += 50;
-			}
+            // 💡 Remember TypeScript only exists at compile time, so when this code *runs* the type we want (Mallet)
+            // should appear to be a string with value 'Flamingo'
+            // ⚠️ Unlike strongly typed languages, there's no such thing as `typeof Mallet` in JavaScript.
+            if (typeof mallet === 'string') {
+                if (mallet === 'Flamingo') score += 50;
+            }
 
-			return score === 100;
-		},
-	};
+            return score === 100;
+        },
+    };
 
-	// 👉 FIXME ❌ These parameters to hit() aren't right!
-	if (game && game.hit && game.hit(2468, 8642)) {
-		print('🏆 Success! You scored! 🏆');
-		print('Time to chat with the queen...');
-		return askQuestion('Press ENTER to continue! ', meetTheQueen);
-	} else {
-		print(
-			'😭 Sadly, you lost. And now the queen wants to decapitate you! 😱'
-		);
-		return endAdventure();
-	}
+    // 👉 FIXME ❌ These parameters to hit() aren't right!
+    if (game && game.hit && game.hit(2468, 8642)) {
+        print('🏆 Success! You scored! 🏆');
+        print('Time to chat with the queen...');
+        return askQuestion('Press ENTER to continue! ', meetTheQueen);
+    } else {
+        print(
+            '😭 Sadly, you lost. And now the queen wants to decapitate you! 😱'
+        );
+        return endAdventure();
+    }
 }
